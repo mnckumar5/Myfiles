8f1b5204910ac8c349d6e993023ed1de0bc80052
mnckumar5 <mnc.kumar5@gmail.com>
2023-02-19 17:50:47 +0530

    added new test case for course creation

---
+++ src/test/java/testcases/Createcourse.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import base.Baseclass;
+
+
+public class Createcourse extends Baseclass
+
+{
+	@Test
+	public void Createcourse()
+	{
+		
+	}
+
+}
