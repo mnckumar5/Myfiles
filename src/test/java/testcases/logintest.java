0e2253248754c39b538ada97fa6f099988bff55c
mnckumar5 <mnc.kumar5@gmail.com>
2023-02-19 16:54:49 +0530

    Pushing code to GITHUB

---
+++ src/test/java/testcases/logintest.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import Browserfactory.Browserfactory;
+import base.Baseclass;
+import dataprovider.CustomDataProvider;
+import pages.loginpage;
+
+public class logintest extends Baseclass
+
+{
+    @Test(dataProvider="Logindetails",dataProviderClass=CustomDataProvider.class)
+	public void logintoapplication(String uname,String pwd)
+	{
+		
+		
+		loginpage login=new  loginpage(driver);
+		login.logintoapplication(uname, pwd);
+		
+		
+	}
+}
